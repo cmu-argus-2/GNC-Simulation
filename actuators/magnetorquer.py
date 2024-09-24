8b6edc5b009a1dc0a492e18f7a9d230e168d6377
Amaar Quadri <amaarquadri@gmail.com>
2024-09-24 13:20:47 -0400

    add basic magnetorquer model

---
+++ actuators/magnetorquer.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class Magnetorquers:
+    """
+    Represents all 3 magnetorquers on the satellite.
+    TODO: calculate induced dipole moment based on applied currents and magnetorquer properties.
+    """
+
+    def __init__(self):
+        self.max_induced_dipole_moment = 1  # TODO: placeholder value
+
+    def get_applied_torque(self, induced_dipole_moment, B_external):
+        """
+        Calculates the torque applied by the magnetorquers given
+        the induced dipole moment and the external magnetic field.
+
+        The induced dipole moment is not the direct control input,
+        it will eventually be calculated based on the applied current and magnetorquer properties.
+
+        Given 3 orthogonal magnetorquers we can produce a dipole moment
+        in any direction up to a maximum magnitude.
+
+        :param induced_dipole_moment: The induced dipole moment of the satellite, in the body frame.
+        :param B_external: The external magnetic field due to the Earth, in the body frame.
+        :return: The torque applied by the magnetorquers, in the body frame.
+        """
+        m_norm = np.linalg.norm(induced_dipole_moment)
+        if m_norm > self.max_induced_dipole_moment:
+            induced_dipole_moment *= self.max_induced_dipole_moment / m_norm
+        return np.cross(induced_dipole_moment, B_external)
