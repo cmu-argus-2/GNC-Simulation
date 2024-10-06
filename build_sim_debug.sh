3759f3d48f20fc84ea677b5299598165a1a732c6
Tushaar <tushaar.jain.duke@gmail.com>
2024-10-05 20:54:17 -0400

    automatically downloading naif file if it doesnt alreayd exist

--- build_sim_debug.sh
+++ build_sim_debug.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e # exit when any command fails
+
+# "-O" means otuput to a file with the same name
+# "-C -": see https://stackoverflow.com/questions/11856351/how-to-skip-already-existing-files-when-downloading-with-curl
+curl -O -C - --silent https://ssd.jpl.nasa.gov/ftp/eph/planets/bsp/de440.bsp --output-dir data
+
 mkdir -p build
 cd build/
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-make -j4
+make -j10
