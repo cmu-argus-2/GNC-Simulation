e9b4243ed7842870083e06a11ea411a7779b47a9
Tushaar <tushaar.jain.duke@gmail.com>
2024-10-07 20:52:22 -0400

    ignoring data folder

--- build_sim_debug.sh
+++ build_sim_debug.sh
@@ -3,6 +3,8 @@ set -e # exit when any command fails
 
 # "-O" means otuput to a file with the same name
 # "-C -": see https://stackoverflow.com/questions/11856351/how-to-skip-already-existing-files-when-downloading-with-curl
+mkdir -p data
+echo "Downloading physics model data files (may take a few minutes) ..."
 curl -O -C - --silent https://ssd.jpl.nasa.gov/ftp/eph/planets/bsp/de440.bsp --output-dir data
 curl -O -C - --silent https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc --output-dir data
 curl -O -C - --silent https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00011.tpc --output-dir data
