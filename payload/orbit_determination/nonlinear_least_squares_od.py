be50037b28602a6816774c0713fe2354216e5fb3
Amaar Quadri <amaarquadri@gmail.com>
2024-11-04 13:32:54 -0500

    add fit_circular_orbit function to get a better initial guess for OD

--- payload/orbit_determination/nonlinear_least_squares_od.py
+++ payload/orbit_determination/nonlinear_least_squares_od.py
@@ -1,10 +1,14 @@
+from collections.abc import Callable
 import numpy as np
 from scipy.spatial.transform import Rotation
 from scipy.optimize import least_squares
+from scipy.stats import circmean, circvar
 
 from world.math.quaternions import q_inv
 from world.physics.orbital_dynamics import f, f_jac
 from sensors.mock_vision_model import Camera
+
+from brahe.constants import R_EARTH, GM_EARTH
 
 
 class OrbitDetermination:
@@ -23,8 +27,91 @@ class OrbitDetermination:
         self.camera = camera
         self.dt = dt
 
+    @staticmethod
+    def fit_circular_orbit(times: np.ndarray, positions: np.ndarray) -> Callable[[np.ndarray], np.ndarray]:
+        """
+        Fits a circular orbit model to a set of timestamped ECI position estimates.
+        This is used for creating an initial guess for the non-linear least squares problem.
+
+        :param times: The timestamps as a numpy array of shape (n,).
+        :param positions: The position estimates in ECI as a numpy array of shape (n, 3).
+        :return: A function that maps timestamps to position estimates based on the resulting circular orbit model.
+        """
+        assert len(positions.shape) == 2, "positions must be a 2D array"
+        assert positions.shape[1] == 3, "positions must have 3 columns"
+        assert positions.shape[0] >= 3, "There must be at least 3 points"
+        assert len(times.shape) == 1, "times must be a 1D array"
+        assert len(times) == positions.shape[0], "times and positions must have the same length"
+
+        """
+        We want to solve for the unit normal vector of the best fit plane that passes through the origin. 
+        This means we want to minimize np.linalg.norm(positions @ normal) subject to np.linalg.norm(normal) == 1.
+        This is solved by computing the right singular vector corresponding to the smallest singular value of positions.
+        """
+        *_, Vt = np.linalg.svd(positions)
+        normal = Vt[-1, :]
+
+        projected_positions = positions - np.outer(positions @ normal, normal)
+        orbital_radius = np.mean(np.linalg.norm(projected_positions, axis=1))
+        speed = np.sqrt(GM_EARTH / orbital_radius)
+        angular_speed = speed / orbital_radius
+
+        # choose basis vectors for the orbital plane
+        # there are 2 cases to avoid scenarios where the cross product is close to zero
+        if abs(normal[0]) <= abs(normal[1]):
+            # normal is closer to the y-axis, so we will use the x-axis in the cross product
+            y_axis = np.cross(normal, np.array([1, 0, 0]))
+            y_axis = y_axis / np.linalg.norm(y_axis)
+            if y_axis[1] < 0:
+                y_axis = -y_axis
+            x_axis = np.cross(y_axis, normal)
+        else:
+            # normal is closer to the x-axis, so we will use the y-axis in the cross product
+            x_axis = np.cross(np.array([0, 1, 0]), normal)
+            x_axis = x_axis / np.linalg.norm(x_axis)
+            if x_axis[0] < 0:
+                x_axis = -x_axis
+            y_axis = np.cross(normal, x_axis)
+
+        positions_2d = projected_positions @ np.column_stack((x_axis, y_axis))
+        angles = np.arctan2(positions_2d[:, 1], positions_2d[:, 0])
+
+        # case 1: the orbit is counter-clockwise in the chosen basis
+        phases_ccw = angles - angular_speed * times
+
+        # case 2: the orbit is clockwise in the chosen basis
+        phases_cw = angles + angular_speed * times
+
+        # choose the case with the smaller variance
+        if circvar(phases_ccw) < circvar(phases_cw):
+            angular_velocity = angular_speed
+            angle_0 = circmean(phases_ccw)
+        else:
+            angular_velocity = -angular_speed
+            angle_0 = circmean(phases_cw)
+
+        def model(ts: np.ndarray) -> np.ndarray:
+            """
+            Maps timestamps to state estimates.
+
+            :param ts: The timestamps to map to state estimates as a numpy array of shape (m,).
+            :return: The resulting state estimates as a numpy array of shape (m, 6).
+            """
+            angles_ = angular_velocity * ts + angle_0
+            positions_2d_ = orbital_radius * np.column_stack((np.cos(angles_), np.sin(angles_)))
+            positions_ = positions_2d_ @ np.row_stack((x_axis, y_axis))
+
+            velocity_directions_ = np.sign(angular_speed) * np.cross(normal, positions_)
+            velocity_directions_ = velocity_directions_ / np.linalg.norm(velocity_directions_, axis=1, keepdims=True)
+            velocities_ = speed * velocity_directions_
+
+            return np.column_stack((positions_, velocities_))
+
+        return model
+
     def fit_orbit(self, times: np.ndarray, landmarks: np.ndarray, pixel_coordinates: np.ndarray,
-                  cubesat_attitudes: np.ndarray, N: int = None) -> np.ndarray:
+                  cubesat_attitudes: np.ndarray, N: int = None,
+                  semi_major_axis_guess: float = R_EARTH + 600e3) -> np.ndarray:
         """
         Solve the orbit determination problem using non-linear least squares.
 
@@ -34,6 +121,7 @@ class OrbitDetermination:
         :param pixel_coordinates: A numpy array of shape (m, 2) containing the pixel coordinates of the landmarks.
         :param cubesat_attitudes: A numpy array of shape (m, 4) containing the quaternions representing the attitude of the satellite.
         :param N: The number of time steps. If None, it will be set to the maximum value in times plus one.
+        :param semi_major_axis_guess: An initial guess for the semi-major axis of the satellite's orbit.
         :return: A numpy array of shape (N, 6) containing the ECI position and velocity of the satellite at each time step.
         """
         assert len(times.shape) == 1, "times must be a 1D array"
@@ -109,6 +197,9 @@ class OrbitDetermination:
 
             return jac
 
-        initial_guess = np.ones(6 * N)  # TODO: improve initial guess
+        altitude_normalized_landmarks = landmarks / np.linalg.norm(landmarks, axis=1, keepdims=True)
+        model = OrbitDetermination.fit_circular_orbit(times, semi_major_axis_guess * altitude_normalized_landmarks)
+        initial_guess = model(np.arange(N) * self.dt).flatten()
+
         result = least_squares(residuals, initial_guess, method="lm", jac=residual_jac)
         return result.x.reshape(N, 6)
