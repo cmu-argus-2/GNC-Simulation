99d87e4ad254db77b1a482cd6ae0195f47b7fe57
kmkar <holophrasis01@gmail.com>
2024-11-11 22:37:03 -0500

    Making sim depndency installation CLI safe

--- sim_installer.sh
+++ sim_installer.sh
@@ -4,7 +4,7 @@ set -e # exit when any command fails
 # For First time installation
 git submodule update --recursive --init
 
-sudo apt install python3-tk -y
-sudo apt install cmake -y
-sudo apt install gfortran -y
-sudo apt install libeigen3-dev -y
+sudo apt-get install python3-tk -y
+sudo apt-get install cmake -y
+sudo apt-get install gfortran -y
+sudo apt-get install libeigen3-dev -y
