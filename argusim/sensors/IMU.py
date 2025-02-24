07b3931307e5dd4b4dd74b97be1f371d71782b88
Amaar Quadri <amaarquadri@gmail.com>
2025-02-24 09:35:06 -0500

    remove duplicate code (#51)

--- argusim/sensors/IMU.py
+++ argusim/sensors/IMU.py
@@ -28,20 +28,3 @@ class IMU:
         gyro_measurement = self.gyro.update(clean_gyro_signal)
         accel_measurement = self.accel.update(clean_accel_signal)
         return gyro_measurement, accel_measurement
-
-
-
-class IMU:
-    def __init__(self, dt, IMU_noise_params):
-        self.gyro = TriAxisSensor(dt, IMU_noise_params.gyro)
-        self.accel = TriAxisSensor(dt, IMU_noise_params.accel)
-
-    def get_bias(self):
-        gyro_bias = self.gyro.get_bias()
-        accel_bias = self.accel.get_bias()
-        return gyro_bias, accel_bias
-
-    def update(self, clean_gyro_signal, clean_accel_signal):
-        gyro_measurement = self.gyro.update(clean_gyro_signal)
-        accel_measurement = self.accel.update(clean_accel_signal)
-        return gyro_measurement, accel_measurement
