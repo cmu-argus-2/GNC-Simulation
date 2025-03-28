005a607a342db29603b9147c9ac2739db4d41ebf
Karthik Karumanchi <kkaruman@andrew.cmu.edu>
2025-03-28 16:07:25 -0400

    Adding Optional Logging

--- argusim/simulation_manager/sim.py
+++ argusim/simulation_manager/sim.py
@@ -18,13 +18,14 @@ import os
 import yaml
 
 class Simulator():
-    def __init__(self, trial_number, log_directory, config_path) -> None:
+    def __init__(self, trial_number, log_directory, config_path, log=True) -> None:
         
         self.trial_number = trial_number
 
         # Datapaths
         self.config_path = config_path
         self.log_directory = log_directory
+        self.log = log
         
         # Spacecraft Config
         self.params = SimParams(self.config_path, self.trial_number, self.log_directory)
@@ -43,17 +44,18 @@ class Simulator():
         self.control_input = np.zeros((self.params.num_MTBs + self.params.num_RWs))
 
         # Logging
-        self.logr = MultiFileLogger(log_directory)
-        self.state_labels = ["r_x ECI [m]", "r_y ECI [m]", "r_z ECI [m]", "v_x ECI [m/s]", "v_y ECI [m/s]", "v_z ECI [m/s]",
-                             "q_w", "q_x", "q_y", "q_z", "omega_x [rad/s]", "omega_y [rad/s]", "omega_z [rad/s]", 
-                             "rSun_x ECI [m]","rSun_y ECI [m]","rSun_z ECI [m]","xMag ECI [T]","yMag ECI [T]","zMag ECI [T]"] + \
-                            ["omega_RW_" + str(i) + " [rad/s]" for i in range(self.num_RWs)]
-        
-        self.measurement_labels = ["gps_posx ECEF [m]", "gps_posy ECEF [m]", "gps_posz ECEF [m]", "gps_velx ECEF [m/s]", "gps_vely ECEF [m/s]", "gps_velz ECEF [m/s]",
-                                   "gyro_x [rad/s]", "gyro_y [rad/s]", "gyro_z [rad/s]", "mag_x_body [T]", "mag_y_body [T]", "mag_z_body [T]"] + \
-                                  ['light_sensor_lux ' + str(i) for i in range(self.num_photodiodes)]
-        
-        self.input_labels = ["V_MTB_" + str(i) + " [V]" for i in range(self.num_MTBs)] + ["T_RW_" + str(i) + " [Nm]" for i in range(self.num_RWs)]
+        if self.log:
+            self.logr = MultiFileLogger(log_directory)
+            self.state_labels = ["r_x ECI [m]", "r_y ECI [m]", "r_z ECI [m]", "v_x ECI [m/s]", "v_y ECI [m/s]", "v_z ECI [m/s]",
+                                "q_w", "q_x", "q_y", "q_z", "omega_x [rad/s]", "omega_y [rad/s]", "omega_z [rad/s]", 
+                                "rSun_x ECI [m]","rSun_y ECI [m]","rSun_z ECI [m]","xMag ECI [T]","yMag ECI [T]","zMag ECI [T]"] + \
+                                ["omega_RW_" + str(i) + " [rad/s]" for i in range(self.num_RWs)]
+            
+            self.measurement_labels = ["gps_posx ECEF [m]", "gps_posy ECEF [m]", "gps_posz ECEF [m]", "gps_velx ECEF [m/s]", "gps_vely ECEF [m/s]", "gps_velz ECEF [m/s]",
+                                    "gyro_x [rad/s]", "gyro_y [rad/s]", "gyro_z [rad/s]", "mag_x_body [T]", "mag_y_body [T]", "mag_z_body [T]"] + \
+                                    ['light_sensor_lux ' + str(i) for i in range(self.num_photodiodes)]
+            
+            self.input_labels = ["V_MTB_" + str(i) + " [V]" for i in range(self.num_MTBs)] + ["T_RW_" + str(i) + " [Nm]" for i in range(self.num_RWs)]
 
     def define_controller(self):
         '''
@@ -132,11 +134,12 @@ class Simulator():
         measurement = self.sensors(current_time, self.state)
         
         # Log pertinent Quantities
-        self.logr.log_v(
-            "state_true.bin",
-            [current_time] + self.state.tolist() + measurement.tolist() + control_input.tolist(),
-            ["Time [s]"] + self.state_labels + self.measurement_labels + self.input_labels
-        )
+        if self.log:
+            self.logr.log_v(
+                "state_true.bin",
+                [current_time] + self.state.tolist() + measurement.tolist() + control_input.tolist(),
+                ["Time [s]"] + self.state_labels + self.measurement_labels + self.input_labels
+            )
 
         return measurement
     
