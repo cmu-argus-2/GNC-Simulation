fa4be5559aa83273bcf426bf7296d8de7c9454fc
prchall123 <prcachim@gmail.com>
2025-08-21 23:58:51 -0400

    igrf 13 to 14

--- argusim/build_sim_debug.sh
+++ argusim/build_sim_debug.sh
@@ -16,7 +16,7 @@ echo "Downloading physics model data files (may take a few minutes) ..."
 curl -O -C - --silent https://ssd.jpl.nasa.gov/ftp/eph/planets/bsp/de440.bsp --output-dir data
 curl -O -C - --silent https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc --output-dir data
 curl -O -C - --silent https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00011.tpc --output-dir data
-curl -O -C - --silent https://www.ngdc.noaa.gov/IAGA/vmod/igrf13.f --output-dir data
+curl -O -C - --silent https://www.ngdc.noaa.gov/IAGA/vmod/igrf14.f --output-dir data
 curl -O -C - --silent https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls --output-dir data
 
 mkdir -p build
